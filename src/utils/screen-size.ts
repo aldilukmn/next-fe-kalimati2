4a925195fd8ba577ddf26ab7d7d9a1a3e0d11bac
aldilukmn <aldilukman99@gmail.com>
2025-02-05 16:54:05 +0700

    fix: window undefined prob

--- src/utils/screen-size.ts
+++ src/utils/screen-size.ts
@@ -2,22 +2,23 @@ import { useEffect, useState } from 'react';
 
 export const screenSize = () => {
   const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
   });
 
   useEffect(() => {
-    const handleResize = () => {
-      setScreenSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    };
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setScreenSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize)
+      };
+    }
   }, []);
 
   return screenSize
