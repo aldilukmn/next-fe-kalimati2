65f64a142200dc46669263357d9da56a08b359a7
aldilukmn <aldilukman99@gmail.com>
2025-02-22 22:15:43 +0700

    update: off middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,34 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server'
-// import { jwtDecode } from 'jwt-decode';
-// import { RoleType } from './app/utils/response/default-response';
+// import type { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server'
+// // import { jwtDecode } from 'jwt-decode';
+// // import { RoleType } from './app/utils/response/default-response';
 
-const isProtectedRoute = (pathname: string) => pathname.startsWith('/admin');
+// const isProtectedRoute = (pathname: string) => pathname.startsWith('/admin');
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('auth_token')?.value;
-  const { pathname } = req.nextUrl;
-  if (!token) {
-    return isProtectedRoute(pathname) ?
-      NextResponse.redirect(new URL('/login', req.url))
-      : NextResponse.next();
-  };
-  // const splitBearer = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
-  // const decoded: RoleType = jwtDecode(splitBearer);
-  // const userTime: number = Number(decoded.exp) * 1000;
-  // const currentTime: number = new Date().getTime();
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get('auth_token')?.value;
+//   const { pathname } = req.nextUrl;
+//   if (!token) {
+//     return isProtectedRoute(pathname) ?
+//       NextResponse.redirect(new URL('/login', req.url))
+//       : NextResponse.next();
+//   };
+//   // const splitBearer = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
+//   // const decoded: RoleType = jwtDecode(splitBearer);
+//   // const userTime: number = Number(decoded.exp) * 1000;
+//   // const currentTime: number = new Date().getTime();
 
-  // if (currentTime > userTime) {
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+//   // if (currentTime > userTime) {
+//   //   return NextResponse.redirect(new URL('/login', req.url));
+//   // }
 
-  if (pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/admin', req.url));
-  }
+//   if (pathname.startsWith('/login')) {
+//     return NextResponse.redirect(new URL('/admin', req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/admin/:path*', '/login'],
-}
+// export const config = {
+//   matcher: ['/admin/:path*', '/login'],
+// }
