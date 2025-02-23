f1635450eba00ef4a73e7c07390a89939df5a9d7
aldilukmn <aldilukman99@gmail.com>
2025-02-23 10:32:06 +0700

    update: middleware change 5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,27 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 import { jwtDecode } from 'jwt-decode';
 import { RoleType } from './app/utils/response/default-response';
 
-const isProtectedRoute = (pathname: string) => pathname.startsWith('/admin');
-
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get('auth_token')?.value;
   const { pathname } = req.nextUrl;
-  if (pathname.startsWith('/admin')) {
-    const token = req.cookies.get('auth_token')?.value;
-      if (!token) {
-        return isProtectedRoute(pathname) ?
-        NextResponse.redirect(new URL('/login', req.url))
-        : NextResponse.next();
-      };
-    const splitBearer = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
-    const decoded: RoleType = jwtDecode(splitBearer);
-    const userTime: number = Number(decoded.exp) * 1000;
-    const currentTime: number = new Date().getTime();
-
-    if (currentTime > userTime) {
+  if (!token) {
+    if (pathname.startsWith('/admin')) {
       return NextResponse.redirect(new URL('/login', req.url));
-    }
+    };
+    return NextResponse.next();
+  };
+
+  const splitBearer = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
+  if (!splitBearer) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+  const decoded: RoleType = jwtDecode(splitBearer);
+  const userTime: number = Number(decoded.exp) * 1000;
+  const currentTime: number = new Date().getTime();
+
+  if (currentTime > userTime) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 
   if (pathname.startsWith('/login')) {
