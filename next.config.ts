f7f515a364ce43f865464d8f5d6fd784c0c08a75
aldilukmn <aldilukman99@gmail.com>
2025-02-23 10:49:06 +0700

    update: next config 2

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.FE_URL },
+          { key: "Access-Control-Allow-Origin", value: "https://uptdsdn2kalimati.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PATCH,DELETE,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
         ]
