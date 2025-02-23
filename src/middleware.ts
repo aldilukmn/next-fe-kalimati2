90b8d5529cc04603c7306e0bc7b4e9f43ddde490
aldilukmn <aldilukman99@gmail.com>
2025-02-23 10:25:20 +0700

    update: middleware change 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,12 +7,27 @@ const isProtectedRoute = (pathname: string) => pathname.startsWith('/admin');
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  if (pathname.startsWith('/admin')) {
-    const token = req.cookies.get('auth_token');
-    if (!token) {
-      return Response.redirect(new URL('/login', req.url));
+  if (pathname.startsWith('/dashboard')) {
+    const token = req.cookies.get('auth_token')?.value;
+      if (!token) {
+        return isProtectedRoute(pathname) ?
+        NextResponse.redirect(new URL('/login', req.url))
+        : NextResponse.next();
+      };
+    const splitBearer = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
+    const decoded: RoleType = jwtDecode(splitBearer);
+    const userTime: number = Number(decoded.exp) * 1000;
+    const currentTime: number = new Date().getTime();
+
+    if (currentTime > userTime) {
+      return NextResponse.redirect(new URL('/login', req.url));
     }
   }
+
+  if (pathname.startsWith('/login')) {
+    return NextResponse.redirect(new URL('/admin', req.url));
+  }
+
   return NextResponse.next();
 }
 
