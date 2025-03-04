1c81c03263fcf15008db68a98f98f43043b53b4a
aldilukmn <aldilukman99@gmail.com>
2025-03-04 21:12:25 +0700

    update: change base url

--- src/app/utils/user/handle-login.ts
+++ src/app/utils/user/handle-login.ts
@@ -3,7 +3,7 @@ import {DefaultResponse} from '../response/default-response';
 import UserType from './type';
 
 export default class handleUser {
-  static baseUrl: string = 'https://user-service-kalimati2.vercel.app/api/users';
+  static baseUrl: string = 'https://pasapiuser.vercel.app/api/users';
   static async doLogin(payload: UserType): Promise<DefaultResponse> {
     const response = await fetch(
       `${this.baseUrl}/login`, {
